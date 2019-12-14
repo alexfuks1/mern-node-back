f9362e28c3b31ec46e93b8e40395ad392ed8f8eb
alex-fuks <alex5825411@gmail.com>
2019-12-14 12:44:16 +0000

    first commit

---
+++ routes/auth.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const {signup,signin,signout} = require('../controllers/auth');
+const {userSignupValidator} = require('../validator/index');
+const {userById} = require('../controllers/user');
+
+const router = express.Router()
+
+router.post("/signup",userSignupValidator,signup);
+router.post("/signin",signin);
+router.get("/signout",signout);
+router.param("userId",userById);
+module.exports = router;
